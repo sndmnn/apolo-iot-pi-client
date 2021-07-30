2a2903b1a56ac98c1075001ab84d70c95a8e7c2a
Henrique Cotta <sndmnn@outlook.com>
2021-07-30 16:38:23 -0300

    added port to url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Socket from './web/Socket.js';
 import TemperatureSensor from './devices/TemperatureSensor.js';
 
-const client = new Socket('http://10.0.0.124');
+const client = new Socket('http://10.0.0.124:3333');
 
 const tempSensor = new TemperatureSensor(11, 4);
 
