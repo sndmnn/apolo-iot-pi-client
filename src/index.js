d8824fb969403fa57f40758537cb866f2bf83d10
Henrique Cotta <sndmnn@outlook.com>
2021-08-02 20:46:36 -0300

    updated server to support two LEDs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import Socket from './web/Socket.js';
 import TemperatureSensor from './devices/TemperatureSensor.js';
+import LED from './devices/LED.js';
 
 const client = new Socket('http://10.0.0.124:3333');
 
 const tempSensor = new TemperatureSensor(11, 4);
 
-(async () => {
+const led1 = new LED(18);
+const led2 = new LED(17);
+
+setInterval(async () => {
   const temp = await tempSensor.read();
 
   client.shout('temp', temp);
-})();
+}, 5000);
+
+client.listen('led-1', (state) => {
+  led1.write(Number(state.state));
+});
+
+client.listen('led-2', (state) => {
+  led2.write(Number(state.state));
+});
